f478ae72177690e33de91e0513261f910324ca93
ultronxr <Ultronxr@users.noreply.github.com>
2023-04-03 13:16:49 +0800

    修复一个BUG：当跨域配置中配置了 `setAllowCredentials(true)` 时，应当使用 `addAllowedOriginPattern("*")` ，而非 `addAllowedOrigin("*")` 。 ``` When allowCredentials is true, allowedOrigins cannot contain the special value "*"since that cannot be set on the "Access-Control-Allow-Origin" response header. To allow credentials to a set of origins, list them explicitly or consider using "allowedOriginPatterns" instead. ```

--- UAIOSC-web/src/main/java/cn/ultronxr/web/config/WebMvcConfigurerAdaptor.java
+++ UAIOSC-web/src/main/java/cn/ultronxr/web/config/WebMvcConfigurerAdaptor.java
@@ -41,7 +41,7 @@ public class WebMvcConfigurerAdaptor implements WebMvcConfigurer {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
