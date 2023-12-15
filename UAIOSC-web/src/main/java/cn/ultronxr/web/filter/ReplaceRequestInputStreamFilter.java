3d931683179b4467a28941a50261cc3708164da2
ultronxr <Ultronxr@users.noreply.github.com>
2023-12-15 14:36:08 +0800

    # UAIOSC-web 修复 request.InputStream() 与 request.getParameter() 互斥的 Bug；Druid 配置迁移。 1. BUG 修复：把 HttpServletRequest 替换为 RequestWrapper 之后，预先读取了 request.InputStream() ，导致后续 request.getParameter() 获取不到登录参数（Druid 登录请求）。     添加了配置项，现在不会再用 RequestWrapper 替换 /druid/ 请求地址开头的请求 2. Druid 配置迁移：把 webStatFilter 、 statViewServlet 相关配置全部迁移到 application-XXX.yml 文件中，不再使用 DruidConfig.java

--- UAIOSC-web/src/main/java/cn/ultronxr/web/filter/ReplaceRequestInputStreamFilter.java
+++ UAIOSC-web/src/main/java/cn/ultronxr/web/filter/ReplaceRequestInputStreamFilter.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Ultronxr
@@ -15,8 +17,28 @@ import java.io.IOException;
 @Slf4j
 public class ReplaceRequestInputStreamFilter implements Filter {
 
+    /**
+     *  如果请求的 URI 地址以下列项开头，则不会把 HttpServletRequest 替换成 {@link RequestWrapper}<br/>
+     *  为了避免某些情况下，RequestWrapper 中读取了输入流，导致后续的 request.getParameter() 方法无法获取参数（例如 Druid 的登录请求）
+     */
+    private static final List<String> IGNORE_REQUEST_URI_PREFIX = Arrays.asList("/druid/");
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // 不拦截文件类型请求
+        if(request.getContentType() == null || request.getContentType().contains("multipart/form-data")) {
+            chain.doFilter(request, response);
+            return;
+        }
+        // 不拦截指定 URI 请求
+        String requestURI = ((HttpServletRequest) request).getRequestURI();
+        for(String ignore : IGNORE_REQUEST_URI_PREFIX) {
+            if(requestURI.startsWith(ignore)) {
+                chain.doFilter(request, response);
+                return;
+            }
+        }
+        // 拦截其他请求，把 HttpServletRequest 替换成 RequestWrapper
         ServletRequest requestWrapper = new RequestWrapper((HttpServletRequest) request);
         chain.doFilter(requestWrapper, response);
     }
