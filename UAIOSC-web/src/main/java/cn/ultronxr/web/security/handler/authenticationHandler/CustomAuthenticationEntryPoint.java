26621c933a95f7844de8ee9d437276e360bd7300
ultronxr <Ultronxr@users.noreply.github.com>
2024-05-04 17:07:57 +0800

    ## UAIOSC-web BUG 修复：[SpringSecurity 组件] CustomAuthenticationEntryPoint 判断 URI 对应处理方法是否存在的逻辑修改，现在可以正常工作了

--- UAIOSC-web/src/main/java/cn/ultronxr/web/security/handler/authenticationHandler/CustomAuthenticationEntryPoint.java
+++ UAIOSC-web/src/main/java/cn/ultronxr/web/security/handler/authenticationHandler/CustomAuthenticationEntryPoint.java
@@ -3,15 +3,20 @@ package cn.ultronxr.web.security.handler.authenticationHandler;
 import cn.ultronxr.common.util.AjaxResponseUtils;
 import cn.ultronxr.common.util.HttpResponseUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * @author Ultronxr
@@ -22,14 +27,24 @@ import java.io.IOException;
 @Slf4j
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    private final RequestMappingHandlerMapping requestMappingHandlerMapping = new RequestMappingHandlerMapping();
+    @Autowired
+    private RequestMappingHandlerMapping requestMappingHandlerMapping;
+
+    @Value("${server.servlet.context-path}")
+    public String serverServletContextPath;
+
 
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
-        // 检查请求的 URI 是否存在，如果不存在，直接返回 404 （SpringSecurity 默认返回 401）
-        if(!requestMappingHandlerMapping.getHandlerMethods().containsKey(request.getRequestURI())) {
+        String uri = request.getRequestURI().replaceFirst(serverServletContextPath, "");
+        Map<RequestMappingInfo, HandlerMethod> handlerMethodMap = requestMappingHandlerMapping.getHandlerMethods();
+
+        // 检查请求的 URI 的对应处理方法（controller）是否存在，如果不存在，直接返回 404 （SpringSecurity 默认返回 401）
+        boolean isContain = handlerMethodMap.keySet()
+                .stream().anyMatch(info -> info.getPatternValues().contains(uri));
+        if(!isContain) {
             log.warn("request = {} , response = {}", request.getRequestURI(), AjaxResponseUtils.HTTP.notFound());
             response.setStatus(HttpStatus.NOT_FOUND.value());
             HttpResponseUtils.sendJson(response, AjaxResponseUtils.HTTP.notFound());
